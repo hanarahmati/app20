66868d842c820c036813db53684d62ad83e522d2
Hana <h.rahmatzehi@behsacorp.com>
2022-10-07 10:04:04 +0330

    session 5 and first part of session 6

---
+++ app/src/main/java/co/fanavari/myapplication20/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package co.fanavari.myapplication20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
