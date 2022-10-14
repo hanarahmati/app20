b89dd4c2a437017714399bb5060a771088b2cd90
Hana <h.rahmatzehi@behsacorp.com>
2022-10-14 11:22:10 +0330

    session 8 navigations

--- app/src/main/java/co/fanavari/myapplication20/FirstFragment.kt
+++ app/src/main/java/co/fanavari/myapplication20/FirstFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import co.fanavari.myapplication20.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentFirstBinding.inflate(layoutInflater,
+            container, false)
+
+        val view = binding.root
+
+        binding.textViewFragment1.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(2)
+            Navigation.findNavController(view).navigate(action)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/co/fanavari/myapplication20/SecondFragment.kt
+++ app/src/main/java/co/fanavari/myapplication20/SecondFragment.kt
@@ -5,15 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import co.fanavari.myapplication20.databinding.FragmentFirstBinding
+import co.fanavari.myapplication20.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val arg: SecondFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(layoutInflater,
+            container, false)
+        val view = binding.root
+        binding.textViewFragment2.text = "fragment 2 got value ${arg.number}"
+
+        binding.textViewFragment2.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
+
+
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
